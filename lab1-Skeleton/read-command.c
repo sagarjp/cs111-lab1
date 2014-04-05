6414edcd41db997f72fa56a3fe0a06f03adef439
Samir Choudhary <choudhar@lnxsrv02.seas.ucla.edu>
2014-04-04 20:30:47 -0700

    complete assignment of tokens

--- lab1-Skeleton/read-command.c
+++ lab1-Skeleton/read-command.c
@@ -3,14 +3,14 @@
 #include "command.h"
 #include "command-internals.h"
 #include "alloc.h"
-#include <ctype.h>
-#include <string.h>
 #include <stdio.h>
 
 #include <error.h>
 
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
+
+#define max_token_size 20
 
 /* FIXME: Define the type 'struct command_stream' here.  This should
    complete the incomplete type declaration in command.h.  */
@@ -29,114 +29,76 @@ enum token_type
   SUBSHELL,
   COMMENT,
   NEWLINE,
-  WHITESPACE,
-  END_OF_FILE,
-  INVALID,
+  WHITESPACE
 };
 
 struct token
 {
-  char *str;
-  enum token_type type;
-  struct token *prev;
-  struct token *next;
+  enum token_type prev_token_type;
+  char* current_token;
+  char next_char;
 };
 
 typedef struct token *token_t;
 
-typedef enum token_type token_type_t;
-
-enum token_type get_token_type(char *str)
+token_t get_next_token(int (*get_next_byte) (void *), void *get_next_byte_argument, token_t prev_t)
 {
-  int i = 0;
-  int len = strlen(str);
-  while(str[i] != '\0')
-  {
-    if(str[i] == ' ' && len == 1)
-    {
-      return WHITESPACE;
-    }
-    else if(str[i] == '\n' && len == 1)
-    {
-      return NEWLINE;
-    }
-    else if(str[i] == EOF && len == 1)
-    {
-      return END_OF_FILE;
-    }
-    //! % + , - . / : @ ^ _
-    else if(isalpha(str[i]) != 0 || isdigit(str[i]) != 0)
-    {
-      i++;
-      continue;
-    }
-    else 
-      return INVALID;
-    i++;
-  }
-  return WORD;
-}
-
-token_t get_next_token(int (*get_next_byte) (void *), void *get_next_byte_argument, struct token *prev)
-{
-  int token_size = 0;
-  int max_token_size = 20;
   int c;
-  char *str = checked_malloc(max_token_size*sizeof(char));
-  str[token_size] = '\0';
-  token_t t1 = (token_t)checked_malloc(sizeof(struct token));
-  token_t t2 = (token_t)checked_malloc(sizeof(struct token));
-
-  while(1)
+  int token_size = 0;
+  //int max_token_size = 20;
+  //char *str = checked_malloc(max_token_size*sizeof(char));
+  //str[token_size] = '\0';
+  //token_t t = (token_t)checked_malloc(sizeof(struct token));
+  
+  for (c = get_next_byte(get_next_byte_argument); ; c = get_next_byte(get_next_byte_argument))
   {
-    c = get_next_byte(get_next_byte_argument);
-    if(c == EOF)
-    {
-      goto return_token;
-    }
-    if(token_size == max_token_size)
+    t -> next_char = c;
+
+    if (c == EOF)
+      return t;
+
+    if (token_size == max_token_size)
     {
       max_token_size += 20;
       str = checked_realloc(str, max_token_size*sizeof(char));
     }
-    if(c == ' ' || c == '\n')
-    {
+
+    if (c == '\n' || c == ' ')
       goto return_token;
-    }
+
     if (c == '#')
     {
-      // if (token_size != 0)
-      //   fprintf(stderr, "error in comment token");
+      if (token_size != 0)
+        fprintf(stderr, "error in comment token");
 
       while (c != '\n')
-      {
         c = get_next_byte(get_next_byte_argument);
-      }
+
+      if (c == '\n')
+        t -> next_char = c;
+
       goto return_token;
     }
+
     str[token_size] = c;
     token_size++;
   }
 
   return_token:
-  t2->str = checked_malloc(2*sizeof(char));
-  t2->str[0] = c;
-  t2->str[1] = '\0';
-  t2->prev = t1;
-  t2->type = get_token_type(t2->str);
-
-  if(token_size == max_token_size)
+  if (token_size == max_token_size)
   {
     max_token_size += 1;
     str = checked_realloc(str, max_token_size*sizeof(char));
   }
+
   str[token_size] = '\0';
-  t1->str = str;
-  t1->prev = prev;
-  prev->next = t1;
-  t1->next = t2;
-  t1->type = get_token_type(t1->str);
-  return t2;
+  t -> str = str;
+  return t;
+}
+
+enum token_type set_token_type(char* a)
+{
+  
 }
 
 command_stream_t make_command_stream(int (*get_next_byte) (void *), void *get_next_byte_argument)
@@ -144,24 +106,8 @@ command_stream_t make_command_stream(int (*get_next_byte) (void *), void *get_ne
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
-  token_t t = checked_malloc(sizeof(struct token));
-  t->prev = NULL;
-  while(1)
-  {
-    token_t temp = get_next_token(get_next_byte, get_next_byte_argument, t);
-    t->next = temp;
-    printf("%s %d\n", temp->prev->str, temp->prev->type);
-    printf("%s %d\n", temp->str, temp->type);
-    if(temp->str[0] == EOF)
-      break;
-    t = temp;
-  }
-  return 0;
-}
+  token_t t = (token_t)checked_malloc(sizeof(struct token));
 
-command_t read_command_stream(command_stream_t s)
-{
-  /* FIXME: Replace this with your implementation too.  */
-  //error (1, 0, "command reading not yet implemented");
-    return 0;
-}
+  t -> prev_token_type = NEWLINE;
+  t -> current_token = checked_malloc(max_token_size*sizeof(char));
+  t -
